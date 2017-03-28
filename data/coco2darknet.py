7cc54b1499aad95d46fde4cf09b3af9faea555f2
zehao <shizehao.scu@gmail.com>
2017-03-28 20:21:09 +0800

    fix bug

--- data/coco2darknet.py
+++ data/coco2darknet.py
@@ -3,8 +3,8 @@ import numpy as np
 from lib import pascal_voc_io
 import os.path as osp
 import cv2
-
-dataDir='/media/zehao/WD/Dataset/processed/COCO'
+import random
+dataDir='/home/zehao/Dataset/COCO'
 dataType='train2014'
 annFile='%s/annotations/instances_%s.json'%(dataDir, dataType)
 
@@ -25,7 +25,6 @@ print 'COCO supercategories: \n', ' '.join(nms)
 catIds = coco.getCatIds(catNms=['person'])
 imgIds = coco.getImgIds(catIds=catIds)
 
-
 for imgId in imgIds:
     img = coco.loadImgs(imgId)[0]
     I = cv2.imread('%s/images/%s/%s' % (dataDir, dataType, img['file_name']))
@@ -39,12 +38,26 @@ for imgId in imgIds:
                 img_shape = np.shape(I)
                 img_h = img_shape[0]
                 img_w = img_shape[1]
-                b_x = (bbox[0] + 0.5*bbox[2])/float(2*img_w)
-                b_y = (bbox[1] + 0.5*bbox[3])/float(2*img_h)
+                b_x = (bbox[0] + 0.5*bbox[2])/float(img_w)
+                b_y = (bbox[1] + 0.5*bbox[3])/float(img_h)
                 b_w = (bbox[2])/float(img_w)
                 b_h = (bbox[3])/float(img_h)
+                """
+                x_ = b_x*img_w
+                y_ = b_y*img_h
+                w_ = b_w*img_w
+                h_ = b_h*img_h
+                x_min = x_ - 0.5 * w_
+                x_max = x_ + 0.5 * w_
+                y_min = y_ - 0.5 * h_
+                y_max = y_ + 0.5 * h_
+                cv2.rectangle(I, (int(bbox[0]), int(bbox[1])), (int(bbox[0]+bbox[2]), int(bbox[1]+bbox[3])), (255, 0, 0), 2)
+                cv2.rectangle(I, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (0,255,255), 2)
+                cv2.imshow('show', I)
+                cv2.waitKey(0)
+                """
                 f.write(cls + ' ' + str(b_x) + ' ' + str(b_y) + ' ' + str(b_w) + ' ' + str(b_h) + '\n')
-    cv2.imwrite(osp.join(timgDir, img['file_name']), I)
+    #cv2.imwrite(osp.join(timgDir, img['file_name']), I)
 
 
 
